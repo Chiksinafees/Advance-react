9f3726640504418aa576e7b3e60cddd052bcfa5b
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-11-09 21:24:17 +0530

    state inside new component task5

--- src/component/NewExpense/ExpenseForm .js
+++ src/component/NewExpense/ExpenseForm .js
@@ -1,34 +1,57 @@
-import React from 'react'
-import './ExpenseForm.css'
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
-const ExpenseForm =() =>{
+const ExpenseForm = () => {
+  const [enteredtitle, setEnteredtitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-   const submitHandler=(e)=>{
-      e.preventDefault()
-      console.log(e.target.value)
-   }
+  const titleHandler = (e) => {
+    e.preventDefault();
+    setEnteredtitle(e.target.value);
+  };
 
-   return (
+  const amountHandler = (e) => {
+    e.preventDefault();
+    setEnteredAmount(e.target.value);
+  };
+
+  const dateHandler = (e) => {
+    e.preventDefault();
+    setEnteredDate(e.target.value);
+  };
+
+  return (
     <form>
       <div className="new-expense__controls ">
         <div className="new-expense__control">
-           <label>Title</label>
-           <input type='text'  onChange={submitHandler}/>
+          <label>Title</label>
+          <input type="text" onChange={titleHandler} />
         </div>
         <div className="new-expense__control">
-           <label>Amount</label>
-           <input type='number' min="0.01"  step="0.01"  onChange={submitHandler} />
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountHandler}
+          />
         </div>
         <div className="new-expense__control">
-           <label>Date</label>
-           <input type='date' min='2022-00-01' max='2022-11-31'  onChange={submitHandler} />
+          <label>Date</label>
+          <input
+            type="date"
+            min="2022-00-01"
+            max="2022-11-31"
+            onChange={dateHandler}
+          />
         </div>
         <div className="new-expense__actions">
-           <button>Add Expense</button>
+          <button>Add Expense</button>
         </div>
-     </div>
+      </div>
     </form>
-   )
-}
+  );
+};
 
-export default ExpenseForm 
+export default ExpenseForm;
