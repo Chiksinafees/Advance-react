0cda9bc750bd1287d235811eeb200ec164af6ba8
Chiksinafees <nafeesahmed01.na@gmail.com>
2022-11-09 14:57:21 +0530

    manipulating dom state using useState task3

--- src/component/expenses/ExpenseItem.js
+++ src/component/expenses/ExpenseItem.js
@@ -1,29 +1,38 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
 const Expense = (props) => {
-    
+  const [title, setTitle] = useState(props.title); //  title= old title     setTitle= new value
+  const [amount, setAmount] = useState(props.amount);
+
   const titleHandler = () => {
-    console.log("u just clicked!!");
+    setTitle("just updated");
+    //console.log(title);
+  };
+  const amountHandler = () => {
+    setAmount("100");
+   // console.log(amount);
   };
 
   const deleteHandler = () => {
-    console.log("u just clicked deleteHandler !!");
+    //console.log("u just clicked deleteHandler !!");
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <h2>{props.LocationOfExpenditure}</h2>
 
       <div>
-        <ExpenseDetails amount={props.amount} />
+        <ExpenseDetails amount={amount} />
       </div>
       <button onClick={titleHandler}>Change Title</button>
+      <button onClick={amountHandler}>Chnge amount</button>
       <button onClick={deleteHandler}>Delete Expense </button>
     </Card>
   );
